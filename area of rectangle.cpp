4d2d670388437fc0e586b05218e5148d8734c37f
Yousuf-Sh <56998778+Yousuf-Sh@users.noreply.github.com>
2020-02-18 11:43:40 +0500

    Finds area of rectangle

---
+++ area of rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ main ()
+ {
+  int l,w,a,perm;
+  cout<<"Enter length and width of rectangle";
+  cin>>l;
+  cin>>w;
+  a=l*w;
+  perm=2*(l+w);
+  cout<<"\nLength = "<<l<<"\nWidth = "<<w;
+  cout<<"\nAREA = "<<a<<"\nPerimiter "<<perm;
+ }
+
